78152bd74019f5d1d663cd8fda167915f9339c0b
Lucassyl <143101598+Lucassyl@users.noreply.github.com>
2025-02-27 17:37:02 -0500

    Ajout du workflow et de la section header troisieme tentative

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+//import "bootstrap/dist/css/bootstrap.min.css"
+//import "bootstrap"
+</script>
+
+<template>
+    <header class="bg-danger p-3">
+        <h1>Guide du joueur - Créer son perso</h1>
+        <section id="banniere">
+              <img class="img-fluid d-none d-lg-block" src="../assets/image/banner1.jpg" alt="Bannière qui affiche différents personnage de DnD">
+        </section>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
